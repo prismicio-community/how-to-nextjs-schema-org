6eb438d05dfe925e0c8c513e2837261f93b950b2
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-09-09 11:14:36 -1000

    feat: add Article example

---
+++ pages/articles/[uid].js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import * as prismicH from "@prismicio/helpers";
+import { createClient } from "../../prismicio";
+
+/** @param {import("next").InferGetStaticPropsType<typeof getStaticProps>} */
+export default function Page({ article, author }) {
+  /** @type {import('schema-dts').Article} */
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    author: {
+      "@type": "Person",
+      name: prismicH.asText(author.data.name),
+      url: new URL(prismicH.asLink(author), "https://example.com"),
+    },
+    image: article.data.featured_image.url,
+    datePublished: article.data.publication_date,
+    dateModified: article.last_publication_date,
+  };
+
+  return (
+    <div>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
+      </Head>
+      <article>{/* The article's contents... */}</article>
+    </div>
+  );
+}
+
+/** @param {import("next").GetStaticPropsContext<{ uid: string }>} */
+export async function getStaticProps({ previewData, params }) {
+  const client = createClient({ previewData });
+
+  const article = await client.getByUID("article", params.uid);
+  const author = await client.getByUID("author", article.data.author.uid);
+
+  return {
+    props: { article, author },
+  };
+}
+
+/** @type {import("next").GetStaticPaths} */
+export async function getStaticPaths() {
+  const client = createClient();
+
+  const articles = await client.getAllByType("article");
+
+  return {
+    paths: articles.map((article) => prismicH.asLink(article)),
+    fallback: true,
+  };
+}
---
+++ prismicio.js
@@ -0,0 +1,38 @@
+import * as prismic from "@prismicio/client";
+import * as prismicNext from "@prismicio/next";
+
+import sm from "./sm.json";
+
+/**
+ * The project's Prismic repository name.
+ */
+export const repositoryName = prismic.getRepositoryName(sm.apiEndpoint);
+
+/** @type {import("@prismicio/client").Route[]} */
+const routes = [
+  {
+    type: "article",
+    path: "/articles/:uid",
+  },
+  {
+    type: "author",
+    path: "/authors/:uid",
+  },
+];
+
+/**
+ * Creates a Prismic client for the project's repository. The client is used to
+ * query content from the Prismic API.
+ *
+ * @param config {prismicNext.CreateClientConfig} - Configuration for the Prismic client.
+ */
+export const createClient = ({ previewData, req, ...config } = {}) => {
+  const client = prismic.createClient(sm.apiEndpoint, {
+    routes,
+    ...config,
+  });
+
+  prismicNext.enableAutoPreviews({ client, previewData, req });
+
+  return client;
+};
